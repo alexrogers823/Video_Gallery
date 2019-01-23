f6374a07eaccc850750db99119192fe382fda27e
Alex Rogers <alex.rogers823@gmail.com>
2019-01-22 20:40:13 -0600

    added fields for different models

--- mysite/videos/models.py
+++ mysite/videos/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.model):
+    title = models.CharField() #fill in something here
+    source_url = models.CharField? #Check documentation on this
+    embed_link = models.CharField? # Same as above
+    type_id = models.ForeignKey() # Type, more on this later
+
+
+class Type(models.model):
+    name = models.CharField()
+
+
+class Rules(models.model):
+    rule = models.CharField()
+
+
+class Solstice(models.model):
+    period = models.CharField() #Example: 'Summmer', 'Winter'
+    year = models.CharField()
+    title = models.CharField()
+    description = models.CharField()
